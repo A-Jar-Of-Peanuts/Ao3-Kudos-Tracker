1d237cd15236825ea8b2f1d9aac89bd410d767ce
A-Jar-Of-Peanuts <33380913+A-Jar-Of-Peanuts@users.noreply.github.com>
2022-08-24 12:12:03 -0700

    Delete content.php

--- AO3 kudos:comments/content.php
+++
@@ -1,8 +0,0 @@
-//  sends message to background.js if user hits kudos
-        //   if (isset($_POST['commit'])) {
-        //     if ($_POST['commit'] == 'Kudos ♥''){
-        //       echo "yes";
-        //     }
-        //   }
-        //   return "no!";
-        //   console.log("this is a php file");
